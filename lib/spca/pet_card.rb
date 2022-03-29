915ce56179d6211badfac376558bdcc214d3ac3d
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-03-29 16:39:16 -0400

    Create pet_card.rb

---
+++ lib/spca/pet_card.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module SPCA
+  class PetCard
+    attr_reader :title, :uri, :image, :info
+
+    def initialize(
+      title:,
+      uri:,
+      image:,
+      info:
+    )
+      @title = title
+      @uri = uri
+      @image = image
+      @info = info
+    end
+
+    def hash
+      Digest::MD5.hexdigest(Marshal.dump(self))
+    end
+
+    def ==(other)
+      PetCard === other &&
+        @title == other.title &&
+        @image == other.image &&
+        @uri == other.uri &&
+        @info == other.info
+    end
+
+    # Create from Nokogiri::XML::Element for .pet--card.
+    def self.from_element(element)
+      title = element.at_css('.card--title').content.strip
+      uri = element.at_css('.card--link').attributes['href'].value
+      info = element.at_css('.pet--infos').content.strip
+
+      image = Image.new(
+        uri: element.at_css('img').attributes['src'].value
+      )
+
+      new(
+        title: title,
+        uri: uri,
+        image: image,
+        info: info
+      )
+    end
+  end
+end
