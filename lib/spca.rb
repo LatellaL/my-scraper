a7e1490896dd5bc261260ff9ef113a7af4f45fc4
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-03-29 16:13:33 -0400

    Create spca.rb

---
+++ lib/spca.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'spca/cache'
+require_relative 'spca/fetcher'
+require_relative 'spca/category'
+require_relative 'spca/pet_card'
+require_relative 'spca/pet_list'
+require_relative 'spca/image'
+require_relative 'spca/mail'
+require_relative 'spca/scanner'
+require_relative 'spca/cli'
+
+module SPCA
+  ROOT_PATH = File.expand_path(File.dirname(File.dirname(_FILE_)))
+
+  TIMEZONE = '-05:00'
+
+end
