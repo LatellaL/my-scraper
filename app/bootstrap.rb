c144d8f8c91f0e8684ecb9cfd3e82de4738697b0
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-03-29 16:06:53 -0400

    Update bootstrap.rb

--- app/bootstrap.rb
+++ app/bootstrap.rb
@@ -1,1 +1,16 @@
-Move requires to spca.rb
+# frozen_string_literal: true
+
+require ‘bundler’
+require ‘digest’
+require ‘net/http’
+
+Bundler.require(:default, :development)
+
+require_relative ‘../lib/spca’
+
+# Configuration
+
+Pry.config.pager = false
+
+ENV.merge! Dotenv.load(“#{SPCA::ROOT_PATH}/.env”)
+
